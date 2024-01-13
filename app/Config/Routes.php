a79ebd936a133c6535ff7258c42f67ffc0dcbfda
Pritam <pritamofficial@gmail.com>
2024-01-13 15:47:40 +0530

    setup

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,21 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+$routes->group('back-panel', static function ($routes) {
+
+    
+    $routes->group('/', ['namespace' => 'App\Controllers'], static function ($routes) {
+        $routes->get('dashboard', 'DashboardController::index');
+       
+    });
+});
+/* $routes->group('back-panel', static function ($routes) {
+
+    
+    $routes->group('/', ['namespace' => 'App\Controllers'], static function ($routes) {
+        $routes->get('dashboard', 'DashboardController::index');
+       
+    });
+}); */
---
+++ app/Views/component/back/script.php
@@ -0,0 +1,13 @@
+<!-- JAVASCRIPT -->
+<script src="<?=base_url('')?>back/js/jquery.min.js"></script>
+    <script src="<?=base_url('')?>back/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=base_url('')?>back/js/metisMenu.min.js"></script>
+    <script src="<?=base_url('')?>back/js/simplebar.min.js"></script>
+    <script src="<?=base_url('')?>back/js/waves.min.js"></script>
+
+    <!-- apexcharts -->
+    <script src="<?=base_url('')?>back/js/apexcharts.min.js"></script>
+
+
+    <!-- App js -->
+    <script src="<?=base_url('')?>back/js/app.js"></script>
