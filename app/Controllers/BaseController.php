d1170187b54a08d5e83961742511efd8c3b4538f
Pritam <pritamofficial@gmail.com>
2024-01-26 20:46:46 +0530

    basic complete

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -82,4 +82,109 @@ abstract class BaseController extends Controller
          session()->setFlashdata('message', NULL);
        }
    }
+
+
+   public function fileHandle($crud = NULL, $field = NULL, $file_type = NULL){
+      if(!is_null($crud)){
+          switch ($file_type) {
+            case 'image':
+              $accept = ".jpg, .jpeg, .png";
+              break;
+            case 'document':
+              $accept = ".pdf, .doc, .docx";
+              break;
+            case 'video':
+              $accept = ".mp4";
+              break;
+            
+            default:
+              $accept = NULL;
+              break;
+          }
+        $crud->callbackColumn($field, array($this, 'showFile'));
+        $crud->callbackAddField(
+          $field,
+          function () use ($accept) {
+              return  '<input id="field-image" type="file" class="form-control w-100 " accept="' . $accept . '" name="image" value="">';
+          }
+      );
+
+      $crud->callbackEditField(
+        $field,
+        function ($data)  use ($field) {
+            $path = base_url() . 'uploads/' . $data;
+
+            $html = $this->showFile($data);
+            $html .= '<input id="field-image" type="file" class="form-control mt-2" accept=".jpg, .jpeg, .png" name="' . $field . '" value="">';
+
+            $html .= '<input id="field-image" type="hidden" class="form-control" name="file_hidden" value="' . $data . '">';
+            return $html;
+        }
+      );
+
+      $crud->callbackBeforeInsert(
+        function ($cbData) use ($field) {
+            $toUpload = $this->request->getFile($field);
+            if (isset($toUpload)) {
+                $image = UploadFile($toUpload);
+                $cbData->data[$field] = $image;
+
+                return $cbData;
+            }
+        }
+      );
+
+      $crud->callbackBeforeUpdate(
+        function ($cbData)  use ($field) {
+            $toUpload = $this->request->getFile($field);
+
+            $file_hidden = $this->request->getVar('file_hidden');
+
+            if (isset($toUpload)) {
+                $image = UploadFile($toUpload, null, $file_hidden);
+                $cbData->data['image'] = $image;
+            } else {
+                $cbData->data['image'] = $file_hidden;
+            }
+            $cbData->data['updated_by'] = getUserData()->id;
+            $cbData->data['updated_at'] = \getCurrentDate();
+            return $cbData;
+        }
+      );
+      $crud->fieldType('updated_by', 'hidden', null);
+      $crud->fieldType('updated_at', 'hidden', null);
+      return $crud;
+      }
+
+      return $crud;
+   }
+
+   public function showFile($value)
+    {
+        $url = base_url('uploads/');
+        $type = pathinfo($value)['extension'];
+        // die($type);
+        $icon = '';
+        if($type === 'pdf'){
+            $icon = '<i class=" fas fa-file-pdf text-danger  fs-1  "></i>';
+        }elseif($type === 'xls' || $type === 'xlsx'){
+            $icon = '<i class="fas fa-file-excel text-success  fs-1  "></i>';
+        }elseif($type === 'doc' || $type === 'docx'){
+            $icon = '<i class=" fas fa-file-word  text-primary   fs-1  "></i>';
+        }elseif($type === 'txt'){
+                $icon = '<i class="fas fa-images fs-1"></i>';
+        }else{
+            $icon = '<i class="fas fa-images"></i>';
+        }
+        
+        // $icon = $type;
+        if($type == 'pdf' or $type == 'xls' or $type == 'xlsx' or $type == 'doc' or $type == 'docx' or $type == 'txt'){
+
+            $path = '<a target="_new" href="'.$url . $value.'"> '. $icon .' </a>';
+        }elseif($type == 'png' or $type == 'jpg' or $type == 'jpeg' or $type == 'bmp' or $type == 'webp' or $type == 'gif'){
+            $path = '<img src=' . $url . $value . ' height="100" width="100">'; 
+        }
+        
+         return $path;
+    }
 }
--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -11,100 +11,16 @@ class HomeController extends BaseController
     public function __construct(Type $var = null) {
         $this->websiteModel = new WebsiteModel();
     }
-    public function showFile($value)
-    {
-        $url = base_url('uploads/');
-        $type = pathinfo($value)['extension'];
-        // die($type);
-        $icon = '';
-        if($type === 'pdf'){
-            $icon = '<i class=" fas fa-file-pdf text-danger  fs-1  "></i>';
-        }elseif($type === 'xls' || $type === 'xlsx'){
-            $icon = '<i class="fas fa-file-excel text-success  fs-1  "></i>';
-        }elseif($type === 'doc' || $type === 'docx'){
-            $icon = '<i class=" fas fa-file-word  text-primary   fs-1  "></i>';
-        }elseif($type === 'txt'){
-                $icon = '<i class="fas fa-images fs-1"></i>';
-        }else{
-            $icon = '<i class="fas fa-images"></i>';
-        }
-        
-        // $icon = $type;
-        if($type == 'pdf' or $type == 'xls' or $type == 'xlsx' or $type == 'doc' or $type == 'docx' or $type == 'txt'){
-
-            $path = '<a target="_new" href="'.$url . $value.'"> '. $icon .' </a>';
-        }elseif($type == 'png' or $type == 'jpg' or $type == 'jpeg' or $type == 'bmp' or $type == 'webp' or $type == 'gif'){
-            $path = '<img src=' . $url . $value . ' height="100" width="100">'; 
-        }
-        
-         return $path;
-    }
+    
     public function Slider(){
         $crud = new GroceryCrud();
         $crud->displayAs('image','Slider');
         $crud->displayAs('is_active','Status');
         $crud->where("deleted_at", NULL);
         $crud->columns(['image', 'is_active']);
-        $crud->fields(['image', 'is_active','created_by','updated_by']);
-        // $crud->setFieldUpload(['image', 'is_active','created_by']);
-        $crud->callbackColumn('image', array($this, 'showFile'));
+        $crud->fields(['image', 'is_active','created_by','updated_at','updated_by']);
         $crud->fieldType('created_by', 'hidden', getUserData()->id);
-        // echo "<pre>"; print_r($crud->getState()); die;
-        $crud->fieldType('updated_by', 'hidden', null);
-        /* $crud->callbackAfterInsert(function ($stateParameters) {
-            return $this->saveLogData('add','state',$stateParameters->data);
-        }); */
-
-        $crud->callbackAddField(
-            'image',
-            function () {
-                return  '<input id="field-image" type="file" class="form-control w-100 " accept=".jpg, .jpeg, .png" name="image" value="">';
-            }
-        );
-
-        $crud->callbackEditField(
-            'image',
-            function ($data) {
-                $path = base_url() . 'uploads/' . $data;
- 
-                $html = $this->showFile($data);
-                $html .= '<input id="field-image" type="file" class="form-control" accept=".jpg, .jpeg, .png" name="image" value="">';
- 
-                $html .= '<input id="field-image" type="hidden" class="form-control" name="image_hidden" value="' . $data . '">';
-                return $html;
-            }
-        );
-
-        $crud->callbackBeforeUpdate(
-            function ($cbData) {
-                $toUpload = $this->request->getFile('image');
- 
-                $ach_img_var = $this->request->getVar('image_hidden');
- 
-                if (isset($toUpload)) {
-                    $image = UploadFile($toUpload, null, $ach_img_var);
-                    $cbData->data['image'] = $image;
-                } else {
-                    $cbData->data['image'] = $ach_img_var;
-                }
-                $cbData->data['updated_by'] = getUserData()->id;
-                return $cbData;
-            }
-        );
-
-        $crud->callbackBeforeInsert(
-            function ($cbData) {
-                $toUpload = $this->request->getFile('image');
-                if (isset($toUpload)) {
-                    $image = UploadFile($toUpload);
-                    $cbData->data['image'] = $image;
- 
-                    return $cbData;
-                }
-            }
-        );
-
-
+        $this->fileHandle($crud, 'image','image');
         // $crud->unsetDelete();
         // $crud->unsetAdd();
         if ($crud->getState() === 'delete') {
@@ -145,56 +61,14 @@ class HomeController extends BaseController
             return $this->saveLogData('add','state',$stateParameters->data);
         }); */
 
-        $crud->callbackAddField(
-            'file',
-            function () {
-                return  '<input id="field-file" type="file" class="form-control w-100 " accept=".pdf, .doc, .docx" name="file" value="">';
-            }
-        );
-
-        $crud->callbackEditField(
-            'file',
-            function ($data) {
-                $path = base_url() . 'uploads/' . $data;
- 
-                $html = $this->showFile($data);
-                $html .= '<input id="field-file" type="file" class="form-control" accept=".pdf, .doc, .docx" name="file" value="">';
- 
-                $html .= '<input id="field-file" type="hidden" class="form-control" name="file_hidden" value="' . $data . '">';
-                return $html;
-            }
-        );
-
-        $crud->callbackBeforeInsert(
-            function ($cbData) {
-                $toUpload = $this->request->getFile('file');
-                if (isset($toUpload)) {
-                    // echo "<pre>"; print_r($toUpload); die;
-                    $file = UploadFile($toUpload);
-                    $cbData->data['file'] = $file;
- 
-                    return $cbData;
-                }
-            }
-        );
-
-        $crud->callbackBeforeUpdate(
-            function ($cbData) {
-                $toUpload = $this->request->getFile('file');
- 
-                $ach_img_var = $this->request->getVar('file_hidden');
- 
-                if (isset($toUpload)) {
-                    $file = UploadFile($toUpload, null, $ach_img_var);
-                    $cbData->data['file'] = $file;
-                } else {
-                    $cbData->data['file'] = $ach_img_var;
-                }
-                $cbData->data['updated_by'] = \getUserData()->id;
- 
-                return $cbData;
-            }
-        );
+        $crud->fieldType('doc_type', 'dropdown', [
+            'NOTICE' => 'Notice',
+            'TENDER' => 'Tender',
+            'NE' => 'News / Events',
+            'ARS' => 'Anti Ragging Section',
+            'MENU' => 'Nav Menu'
+        ]);
+        $this->fileHandle($crud, 'file','document');
 
         
 
