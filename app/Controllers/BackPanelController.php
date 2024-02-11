6787ce85fe0356669ef3e50e4e88bb142ccd7891
Pritam <pritamofficial@gmail.com>
2024-02-11 23:48:34 +0530

    user set up

--- app/Controllers/BackPanelController.php
+++ app/Controllers/BackPanelController.php
@@ -160,6 +160,8 @@ class BackPanelController extends BaseController
         $crud->columns(['page_type','banner', 'title_1', 'file_1', 'title_2','file_2','title_3','file_3']);
         $crud->fields(['page_type','banner','content','title_1', 'file_1', 'title_2','file_2','title_3','file_3','title_4','file_4','title_5','file_5','title_6','file_6','title_7','file_7','title_8','file_8','created_by']);
         $crud->setTexteditor(['content']);
+        $crud->fieldType('created_by', 'hidden', getUserData()->id);
+        $crud->requiredFields(['page_type']);
         $crud->fieldType('page_type', 'dropdown', [
             '1' => 'Administrative staff',
             '2' => 'OPD SCHEDULE',
@@ -167,18 +169,304 @@ class BackPanelController extends BaseController
             '4' => 'Admission Fees',
             // 'MENU' => 'Nav Menu'
         ]);
-        // if ($crud->getState() === 'add') {
-            $this->fileHandle($crud, 'banner','image');
-            $this->fileHandle($crud, 'file_1',null);
-        // }
-        
-        // $this->fileHandle($crud, 'file_2',null);
-        // $this->fileHandle($crud, 'file_3',null);
-        // $this->fileHandle($crud, 'file_4',null);
-        // $this->fileHandle($crud, 'file_5',null);
-        // $this->fileHandle($crud, 'file_6',null);
-        // $this->fileHandle($crud, 'file_7',null);
-        // $this->fileHandle($crud, 'file_8',null);
+
+        /* Work With File */
+        $crud->callbackColumn('banner', array($this, 'showFile'));
+        $crud->callbackColumn('file_1', array($this, 'showFile'));
+        $crud->callbackColumn('file_2', array($this, 'showFile'));
+        $crud->callbackColumn('file_3', array($this, 'showFile'));
+        $crud->callbackColumn('file_4', array($this, 'showFile'));
+        $crud->callbackColumn('file_5', array($this, 'showFile'));
+        $crud->callbackColumn('file_6', array($this, 'showFile'));
+        $crud->callbackColumn('file_7', array($this, 'showFile'));
+        $crud->callbackColumn('file_8', array($this, 'showFile'));
+
+
+        $crud->callbackAddField(
+            'banner',
+            function () {
+                return  '<input id="field-banner" type="file" class="form-control" name="banner" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_1',
+            function () {
+                return  '<input id="field-file_1" type="file" class="form-control  " name="file_1" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_2',
+            function () {
+                return  '<input id="field-file_2" type="file" class="form-control  " name="file_2" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_3',
+            function () {
+                return  '<input id="field-file_3" type="file" class="form-control  " name="file_3" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_4',
+            function () {
+                return  '<input id="field-file_4" type="file" class="form-control  " name="file_4" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_5',
+            function () {
+                return  '<input id="field-file_5" type="file" class="form-control  " name="file_5" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_6',
+            function () {
+                return  '<input id="field-file_6" type="file" class="form-control  " name="file_6" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_7',
+            function () {
+                return  '<input id="field-file_7" type="file" class="form-control  " name="file_7" value="">';
+            }
+        );
+        $crud->callbackAddField(
+            'file_8',
+            function () {
+                return  '<input id="field-file_8" type="file" class="form-control  " name="file_8" value="">';
+            }
+        );
+
+        $crud->callbackBeforeInsert(
+            function ($cbData) {
+                $banner = $this->request->getFile('banner');
+                if (isset($banner)) {
+                    $file = UploadFile($banner);
+                    $cbData->data['banner'] = $file;
+                }
+                $file_1 = $this->request->getFile('file_1');
+                if (isset($file_1)) {
+                    $file = UploadFile($file_1);
+                    $cbData->data['file_1'] = $file;
+                }
+                $file_2 = $this->request->getFile('file_2');
+                if (isset($file_2)) {
+                    $file = UploadFile($file_2);
+                    $cbData->data['file_2'] = $file;
+                }
+                $file_3 = $this->request->getFile('file_3');
+                if (isset($file_3)) {
+                    $file = UploadFile($file_3);
+                    $cbData->data['file_3'] = $file;
+                }
+                $file_4 = $this->request->getFile('file_4');
+                if (isset($file_4)) {
+                    $file = UploadFile($file_4);
+                    $cbData->data['file_4'] = $file;
+                }
+                $file_5 = $this->request->getFile('file_5');
+                if (isset($file_5)) {
+                    $file = UploadFile($file_5);
+                    $cbData->data['file_5'] = $file;
+                }
+                $file_6 = $this->request->getFile('file_6');
+                if (isset($file_6)) {
+                    $file = UploadFile($file_6);
+                    $cbData->data['file_6'] = $file;
+                }
+                $file_7 = $this->request->getFile('file_7');
+                if (isset($file_7)) {
+                    $file = UploadFile($file_7);
+                    $cbData->data['file_7'] = $file;
+                }
+                $file_8 = $this->request->getFile('file_8');
+                if (isset($file_8)) {
+                    $file = UploadFile($file_8);
+                    $cbData->data['file_8'] = $file;
+                }
+                return $cbData;
+            }
+        );
+
+        $crud->callbackEditField(
+            'banner',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-banner" type="file" class="form-control mt-2" name="banner" value="">';
+
+                $html .= '<input id="field-hidden-banner" type="hidden" class="form-control" name="file_hidden_banner" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_1',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_1" type="file" class="form-control mt-2" name="file_1" value="">';
+
+                $html .= '<input id="field-hidden-file_1" type="hidden" class="form-control" name="file_hidden_file_1" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_2',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_2" type="file" class="form-control mt-2" name="file_2" value="">';
+
+                $html .= '<input id="field-hidden-file_2" type="hidden" class="form-control" name="file_hidden_file_2" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_3',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_3" type="file" class="form-control mt-2" name="file_3" value="">';
+
+                $html .= '<input id="field-hidden-file_3" type="hidden" class="form-control" name="file_hidden_file_3" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_4',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_4" type="file" class="form-control mt-2" name="file_4" value="">';
+
+                $html .= '<input id="field-hidden-file_4" type="hidden" class="form-control" name="file_hidden_file_4" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_5',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_5" type="file" class="form-control mt-2" name="file_5" value="">';
+
+                $html .= '<input id="field-hidden-file_5" type="hidden" class="form-control" name="file_hidden_file_5" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_6',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_6" type="file" class="form-control mt-2" name="file_6" value="">';
+
+                $html .= '<input id="field-hidden-file_6" type="hidden" class="form-control" name="file_hidden_file_6" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_7',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_7" type="file" class="form-control mt-2" name="file_7" value="">';
+
+                $html .= '<input id="field-hidden-file_7" type="hidden" class="form-control" name="file_hidden_file_7" value="' . $data . '">';
+                return $html;
+            }
+        );
+        $crud->callbackEditField(
+            'file_8',
+            function ($data) {
+
+                $html = $this->showFile($data);
+                $html .= '<input id="field-file_8" type="file" class="form-control mt-2" name="file_8" value="">';
+
+                $html .= '<input id="field-hidden-file_8" type="hidden" class="form-control" name="file_hidden_file_8" value="' . $data . '">';
+                return $html;
+            }
+        );
+
+        $crud->callbackBeforeUpdate(
+            function ($cbData) {
+                $banner = $this->request->getFile('banner');
+                $file_hidden_banner = $this->request->getVar('file_hidden_banner');
+                if (isset($banner)) {
+                    $file = UploadFile($banner, null, $file_hidden_banner);
+                    $cbData->data['banner'] = $file;
+                } else {
+                    $cbData->data['banner'] = $file_hidden_banner;
+                }
+                $file_1 = $this->request->getFile('file_1');
+                $file_hidden_file_1 = $this->request->getVar('file_hidden_file_1');
+                if (isset($file_1)) {
+                    $file = UploadFile($file_1, null, $file_hidden_file_1);
+                    $cbData->data['file_1'] = $file;
+                } else {
+                    $cbData->data['file_1'] = $file_hidden_file_1;
+                }
+                $file_2 = $this->request->getFile('file_2');
+                $file_hidden_file_2 = $this->request->getVar('file_hidden_file_2');
+                if (isset($file_2)) {
+                    $file = UploadFile($file_2, null, $file_hidden_file_2);
+                    $cbData->data['file_2'] = $file;
+                } else {
+                    $cbData->data['file_2'] = $file_hidden_file_2;
+                }
+                $file_3 = $this->request->getFile('file_3');
+                $file_hidden_file_3 = $this->request->getVar('file_hidden_file_3');
+                if (isset($file_3)) {
+                    $file = UploadFile($file_3, null, $file_hidden_file_3);
+                    $cbData->data['file_3'] = $file;
+                } else {
+                    $cbData->data['file_3'] = $file_hidden_file_3;
+                }
+                $file_4 = $this->request->getFile('file_4');
+                $file_hidden_file_4 = $this->request->getVar('file_hidden_file_4');
+                if (isset($file_4)) {
+                    $file = UploadFile($file_4, null, $file_hidden_file_4);
+                    $cbData->data['file_4'] = $file;
+                } else {
+                    $cbData->data['file_4'] = $file_hidden_file_4;
+                }
+                $file_5 = $this->request->getFile('file_5');
+                $file_hidden_file_5 = $this->request->getVar('file_hidden_file_5');
+                if (isset($file_5)) {
+                    $file = UploadFile($file_5, null, $file_hidden_file_5);
+                    $cbData->data['file_5'] = $file;
+                } else {
+                    $cbData->data['file_5'] = $file_hidden_file_5;
+                }
+                $file_6 = $this->request->getFile('file_6');
+                $file_hidden_file_6 = $this->request->getVar('file_hidden_file_6');
+                if (isset($file_6)) {
+                    $file = UploadFile($file_6, null, $file_hidden_file_6);
+                    $cbData->data['file_6'] = $file;
+                } else {
+                    $cbData->data['file_6'] = $file_hidden_file_6;
+                }
+                $file_7 = $this->request->getFile('file_7');
+                $file_hidden_file_7 = $this->request->getVar('file_hidden_file_7');
+                if (isset($file_7)) {
+                    $file = UploadFile($file_7, null, $file_hidden_file_7);
+                    $cbData->data['file_7'] = $file;
+                } else {
+                    $cbData->data['file_7'] = $file_hidden_file_7;
+                }
+                $file_8 = $this->request->getFile('file_8');
+                $file_hidden_file_8 = $this->request->getVar('file_hidden_file_8');
+                if (isset($file_8)) {
+                    $file = UploadFile($file_8, null, $file_hidden_file_8);
+                    $cbData->data['file_8'] = $file;
+                } else {
+                    $cbData->data['file_8'] = $file_hidden_file_8;
+                }
+                return $cbData;
+            }
+        );
+
+        /* End */
         $crud->unsetPrint();
         $crud->unsetExport();
         $crud->setTable('content');
